aaab06abd9f8ddc493e2300869cd42663fef4a0a
Tomas Bussolini <tomas.bussolini@students.salle.url.edu>
2022-02-20 19:53:58 +0100

    Actualizando proyecto #1

--- app/src/main/java/com/bussolini/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bussolini/geoquiz/QuizActivity.java
@@ -5,17 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private int count = 0;
+
+    private Boolean[] answers = {
+            false, true, false, true, true, false, true, false, true, true
+    };
+
+    protected boolean checkResponse(boolean answer, int count){
+        boolean response = false;
+
+        if (answers[count].equals(answer))
+            response = true;
+
+        return response;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        TextView question = (TextView) findViewById(R.id.text_question);
+
+        String[] questions = {
+                getString(R.string.question_text1),
+                getString(R.string.question_text2),
+                getString(R.string.question_text3),
+                getString(R.string.question_text4),
+                getString(R.string.question_text5),
+                getString(R.string.question_text6),
+                getString(R.string.question_text7),
+                getString(R.string.question_text8),
+                getString(R.string.question_text9),
+                getString(R.string.question_text10)
+        };
 
         trueButton = (Button) findViewById(R.id.true_button);
 
@@ -23,9 +63,35 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                if (count < 9) {
+
+                    if (checkResponse(true, count)) {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    count++;
+                    question.setText(questions[count]);
+                }
+
+                else if (count == 9){
+                    question.setText(getString(R.string.question_text11));
+                    trueButton.setText(getString(R.string.yes_button));
+                    falseButton.setText(getString(R.string.no_button));
+                    count ++;
+                }
+
+                else{
+                    count = 0;
+                    trueButton.setText(getString(R.string.true_button));
+                    falseButton.setText(getString(R.string.false_button));
+                    question.setText(questions[count]);
+                }
             }
         });
 
@@ -34,11 +100,45 @@ public class QuizActivity extends AppCompatActivity {
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // toco FALSE
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+
+                if (count < 9) {
+
+                    if (checkResponse(false, count)) {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    count++;
+                    question.setText(questions[count]);
+                }
+
+                else if (count == 9){
+                    question.setText(getString(R.string.question_text11));
+                    trueButton.setText(getString(R.string.yes_button));
+                    falseButton.setText(getString(R.string.no_button));
+                    count ++;
+                }
+
+                else
+                    finish();
+
             }
         });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
